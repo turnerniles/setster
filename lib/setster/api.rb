fc593e4b65666587cb0eeb6d2d44a07c6e09199f
Kevin Pheasey <kevin.pheasey@gmail.com>
2015-05-11 14:55:07 -0400

    Abstract the base url into a constant and automatically pass the session token with get and post  requests if it's available.

--- lib/setster/api.rb
+++ lib/setster/api.rb
@@ -1,6 +1,8 @@
 module Setster
   class API
 
+    URL = 'http://www.setster.com/api/v2'
+
     attr_reader :session_token
 
     # Create a new Setster session.
@@ -8,7 +10,7 @@ module Setster
     # @param api_key [String]
     # @return [Setster]
     def initialize(email, api_key)
-      data = post('http://www.setster.com/api/v2/account/authenticate', email: email, token: api_key)
+      data = post("#{URL}/account/authenticate", email: email, token: api_key)
       @session_token = data['session_token']
     end
 
@@ -16,7 +18,7 @@ module Setster
     #
     # @return [Array(Hash)]
     def location_list
-      location_url = "http://www.setster.com/api/v2/location?session_token=#{session_token}"
+      location_url = "#{URL}/location"
       get(location_url)
     end
 
@@ -24,14 +26,15 @@ module Setster
     #
     # @return [Array(Hash)]
     def service_options
-      service_url = "http://www.setster.com/api/v2/service?session_token=#{session_token}"
+      service_url = "#{URL}/service"
       get(service_url)
     end
 
     private
 
-    def get(url)
-      response = HTTParty.get(url, format: :json)
+    def get(url, query = {})
+      query = { session_token: session_token }.merge(query) if session_token
+      response = HTTParty.get(url, query: query, format: :json)
 
       if response.success?
         return response['data']
@@ -40,8 +43,9 @@ module Setster
       end
     end
 
-    def post(url, data = {})
-      response = HTTParty.post(url, query: data)
+    def post(url, query = {})
+      query = { session_token: session_token }.merge(query) if session_token
+      response = HTTParty.post(url, query: query, format: :json)
 
       if response.success?
         return response.parsed_response['data']
